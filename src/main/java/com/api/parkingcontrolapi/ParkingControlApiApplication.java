e60237e2c73bd1e6893b3c343991042f0f02ea17
Patricia Tami Sakita <67209629+tamisakita@users.noreply.github.com>
2022-08-08 11:58:54 -0300

    Initial commit

---
+++ src/main/java/com/api/parkingcontrolapi/ParkingControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.parkingcontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingControlApiApplication.class, args);
+	}
+
+}
